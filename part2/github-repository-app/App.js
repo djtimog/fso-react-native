bb6529ed37bdd787f5d41421544d678fbd0e03d4
Christian Ogunleye <ogunleyetimilehin15@gmail.com>
2026-04-18 19:29:17 +0100

    feat: integrate Apollo Client for GraphQL data fetching
    - Added Apollo Client dependencies to package.json
    - Created .env file to store Apollo URI
    - Configured app.config.js to use environment variables
    - Implemented GraphQL queries in queries.js for fetching repositories
    - Created custom hook useRepositories.js for managing repository data
    - Set up Apollo Client in apolloClient.js for API communication
    - Added empty fragments.js and mutations.js files for future use

--- part2/github-repository-app/App.js
+++ part2/github-repository-app/App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-
+import { ApolloProvider } from "@apollo/client";
 import { NativeRouter } from "react-router-native";
 import Main from "./components/Main";
+import createApolloClient from "./utils/apolloClient";
+import Constants from "expo-constants";
 
 export default function App() {
+  const uri =
+    Constants.expoConfig?.extra?.apolloUri ?? "http://172.20.10.3:4000/graphql";
+  const apolloClient = createApolloClient(uri);
+
   return (
-    <NativeRouter>
-      <Main />
-      <StatusBar style="auto" />
-    </NativeRouter>
+    <ApolloProvider client={apolloClient}>
+      <NativeRouter>
+        <Main />
+        <StatusBar style="auto" />
+      </NativeRouter>
+    </ApolloProvider>
   );
 }
